4e30773741f784b4e13db9500bea76ac56d1237a
Carlos Schellenberger <carlos.schellenberger@gmail.com>
2021-02-04 15:56:46 -0300

    RSA 512 Keys - correcting errors

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -2,15 +2,18 @@ import jwt from 'jsonwebtoken'
 import AppError from './../utils/appError.js'
 
 const signToken = (user) => {
-  const payload = { email: user.email,role: user.role }
+  const payload = { email: user.email, role: user.role }
   // console.log(process.env.JWT_SECRET)
   const tokenGenerated = jwt.sign(
-    payload, 
-    process.env.JWT_SECRET, 
-    { algorithm: ['RS256'], expiresIn: (process.env.JWT_TOKEN_EXPIRES_IN_HOURS * 60 * 60)
-    }, (err, user) => {
+    payload,
+    process.env.JWT_SECRET,
+    {
+      algorithm: ['RS256'],
+      expiresIn: process.env.JWT_TOKEN_EXPIRES_IN_HOURS * 60 * 60,
+    },
+    (err, user) => {
       //console.log(token)
-      return (new AppError('Erro na geração do Token !', 403))
+      return new AppError('Erro na geração do Token !', 403)
     }
   )
   return tokenGenerated
@@ -39,35 +42,35 @@ const signToken = (user) => {
 
 const createSendToken = async (user, statusCode, req, res) => {
   const tokenData = { email: user.email, role: user.role }
-  const tokenOptions = { algorithm: "RS512", expiresIn: 24 * 60 * 60 }
-  const tokenKey = fs.readFileSync("private.key")
+  const tokenOptions = { algorithm: 'RS512', expiresIn: 24 * 60 * 60 }
+  const tokenKey = fs.readFileSync('private.key')
   // console.log(PRIVATE_KEY)
-  let tokenGenerated = ""
+  let tokenGenerated = ''
   tokenGenerated = await jwt.sign(
     tokenData,
     tokenKey,
     tokenOptions,
     (err, token, user, statusCode, req, res) => {
       if (err) {
-        console.log("Erro na geração do Token !", err)
-        return new AppError("Erro na geração do Token !", 500)
+        console.log('Erro na geração do Token !', err)
+        return new AppError('Erro na geração do Token !', 500)
       } else {
-        console.log("Sucesso na geração do Token !", token)
+        console.log('Sucesso na geração do Token !', token)
         // Grava o cookie com o jwt token
-        res.cookie("jwt", token, {
+        res.cookie('jwt', token, {
           expires: new Date(
             Date.now() +
               process.env.JWT_COOKIE_EXPIRES_IN_HOURS * 60 * 60 * 1000
           ),
           httpOnly: true,
-          secure: req.secure || req.headers["x-forwarded-proto"] === "https"
+          secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
         })
         // Remove password from output
         user.password = undefined
         res.status(statusCode).json({
-          status: "success",
+          status: 'success',
           token,
-          user
+          user,
         })
         // return token
       }
